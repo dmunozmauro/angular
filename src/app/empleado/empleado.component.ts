79a3ebeadd85ffcdd249941cf49938e4f85671f7
Gabriel Olave <golavemo@everis.com>
2018-07-09 19:04:50 -0400

    nueva info

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 @Component({
     selector: 'empleado-tag',
     templateUrl: './empleado.component.html'
 })
 
-export class EmpleadoComponent{
-    public titulo = 'Titulo del componente empleado';
-    
+export class EmpleadoComponent {
+    public titulo = 'Componente Empleados';
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+    public trabajadorExterno: boolean;
+
+
+    constructor() {
+        this.empleado = new Empleado('Diego', 28, 'Desarrollador', true);
+        this.trabajadores = [
+            new Empleado('Diego', 28, 'Desarrollador', true),
+            new Empleado('Manuel', 20, 'UX', true),
+            new Empleado('Sofia', 18, 'Arquitecto', false)
+        ];
+        this.trabajadorExterno = true;
+    }
+
+    ngOnInit() {
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor) {
+        this.trabajadorExterno = valor;
+        
+    }
+
 }
